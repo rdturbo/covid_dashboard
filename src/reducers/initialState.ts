c1f3b9575121f1f436e62855f581e8effe815db3
Rupayan Das <rupayan98@gmail.com>
2021-09-30 11:55:56 -0400

    Create initialState.ts

---
+++ src/reducers/initialState.ts
@@ -0,0 +1,13 @@
+let initialState: any = {
+  totalConfirmed: 0,
+  totalDeaths: 0,
+  totalRecovered: 0,
+  summaryIndia: {},
+  allDataIndia: [],
+  summaryUSA: {},
+  allDataUSA: [],
+  allCountriesData: [],
+  initialLoading: true,
+};
+
+export default initialState;
